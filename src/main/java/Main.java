9846d07f71fa5e18103a6669f67b246be6b81212
Melina <melinadanhier@gmail.com>
2023-11-22 16:23:26 +0100

    Main Aenderung 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
+        Car car4 = new Car("blue", "Renault", 4);
+
         factory.buildCar(car1);
         factory.buildCar(car2);
     }
